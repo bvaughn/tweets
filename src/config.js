df899ef4f2ec69556fb87ff63533730e7097c9e0
JP Driver <jpsdriver@gmail.com>
2017-04-26 09:44:56 +0100

    typos in the URLs 🙄

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 function config () {
 	return {
-		"tweetsUrl": process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://tweet-dev.now.sh",
-		"tweetsServerUrl": process.env.NODE_ENV === "development" ? "http://localhost:5001" : "https://tweet-server.now.sh"
+		"tweetsUrl": process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://tweets-dev.now.sh",
+		"tweetsServerUrl": process.env.NODE_ENV === "development" ? "http://localhost:5001" : "https://tweets-server.now.sh"
 	}
 }
 
