8e004d343e8083e13f70c648937c8ed083868eee
Brian Vaughn <bvaughn@fb.com>
2017-04-26 21:47:56 -0700

    Removed unused tweetsUrl config param

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
 function config() {
   return {
-    tweetsUrl: process.env.NODE_ENV === 'development'
-      ? 'http://localhost:5000'
-      : 'https://tweets-dev.now.sh',
     tweetsServerUrl: process.env.NODE_ENV === 'development'
       ? 'http://localhost:5001'
       : 'https://tweets-server.now.sh',
