dec764a82cc6390d5dced2091365ffc80d8bf418
Brian Vaughn <bvaughn@fb.com>
2017-04-29 21:22:35 -0700

    Changed api urls

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ function config() {
   return {
     tweetsServerUrl: process.env.NODE_ENV === 'development'
       ? 'http://localhost:5001'
-      : 'https://tweets-server.now.sh',
+      : 'https://tweets-auth.now.sh',
   };
 }
 
